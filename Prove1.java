1b1ca3bc7c193e2d68a2c422880db09396831bb2
NibeditaDas-Student <saraswatidas8653@gmail.com>
2025-08-30 13:18:04 +0530

    Add files via upload

---
+++ Prove1.java
@@ -0,0 +1,9 @@
+package chapter4.inheritance;
+
+public class Prove1 {
+
+	public static void main(String[] args) {
+		Prove1 obj = new Prove1();
+		System.out.println(obj instanceof Object);
+	}
+}
